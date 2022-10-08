85eb26e0bf48b4992c089f7f90f47f72f303272b
jo0707 <josua123690707@gmail.com>
2022-10-08 21:47:22 +0700

    Fixing responsive layout

--- Bookshelv/assets/js/validator.js
+++
@@ -1,3 +0,0 @@
-export function sanitizeYear(value) {
-    return value.replace(/\D/g, "");
-}
--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -12,18 +12,18 @@ export function createBookElement(book, uncompleted) {
     div.innerHTML = `
     <div class="flex p-4 mt-4 w-full bg-slate-600/75 shadow-sm rounded transition-all hover:scale-[1.01] hover:shadow-xl">
     <div class="grow">
-        <h3 class="text-md">${book.title} (${book.year})</h3>
-        <h3 class="mt-2 text-sm">${book.author}</h3>
+        <h3 class="text-base sm:text-lg">${book.title} (${book.year})</h3>
+        <h3 class="mt-2 text-sm text-slate-200">${book.author}</h3>
     </div>
     
     <div class="my-auto py-auto">
-        <button class="h-8 w-8 inline hover:scale-[1.2] transition-all" title="Delete Book" id="deleteBtn-${book.id}">
+        <button class="h-8 w-8 block sm:inline hover:scale-[1.2] transition-all" title="Delete Book" id="deleteBtn-${book.id}">
             <img src="/Bookshelv/assets/img/delete.png"/>      
         </button>
-        <button class="h-8 w-8 inline hover:scale-[1.2] transition-all" title="Edit Book" id="editBtn-${book.id}">
+        <button class="h-8 w-8 block sm:inline hover:scale-[1.2] transition-all" title="Edit Book" id="editBtn-${book.id}">
             <img src="/Bookshelv/assets/img/edit.png"/>                                                            
         </button>
-        <button class="h-8 w-8 inline hover:scale-[1.2] transition-all" title="Mark as ${uncompleted ? "completed" : "uncompleted"}" id="completeBtn-${book.id}">
+        <button class="h-8 w-8 block sm:inline hover:scale-[1.2] transition-all" title="Mark as ${uncompleted ? "completed" : "uncompleted"}" id="completeBtn-${book.id}">
             <img src="/Bookshelv/assets/img/${uncompleted ? "complete" : "rollback"}.png"/>
         </button>
     </div>
